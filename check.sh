39515e1e825b794da7239c8d0933e0fb7b245909
Kirill Pudov <kereeell@kereeell-MacBook-Pro.local>
2025-12-09 00:49:56 +0300

    fix

--- check.sh
+++ check.sh
@@ -3,7 +3,7 @@ while [ $i -le 4 ];
 do 
     if [ $(kubectl get po -A | grep wordpress | awk '{print $4}') != 'Running' ]; 
     then 
-        export i=$(i + 1) && echo "Iteration $i" && sleep 1m && continue; 
+        i=$(i + 1) && echo "Iteration $i" && sleep 10s && continue; 
     else 
         break
     fi 
