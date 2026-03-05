7741fb0e9f4ada997ba66a0f0c5d48711c9464c7
Kirill Pudov <kereeell@kereeell-MacBook-Pro.local>
2026-03-05 15:42:03 +0300

    add fix

--- check.sh
+++ check.sh
@@ -1,13 +1,13 @@
 set -xe
 i=0
-while [ $i -le 20 ];
+while [ $i -le 100 ];
 do
     STATE=`kubectl get po -A | grep wordpress | awk '{print $4}'`
     if [ $STATE != 'Running' ]; 
     then 
         i=$((i + 1))
         echo "Iteration $i"
-        sleep 10s && continue; 
+        sleep 2s && continue; 
     else 
         break
     fi 
